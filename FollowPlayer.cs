9f2f841c8efe160494eadb6df62776414455550a
Lucas Marins <lucasmarinsramalho@gmail.com>
2021-10-02 01:04:30 -0300

    Create FollowPlayer.cs

---
+++ FollowPlayer.cs
@@ -0,0 +1,17 @@
+/*
+Script da câmera que faz com que ela persiga o jogador
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset; 
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset; 
+    }
+}
