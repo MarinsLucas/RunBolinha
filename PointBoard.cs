a042e07c1fe69f215b1e76478c7ceee8e69b456a
Lucas Marins <lucasmarinsramalho@gmail.com>
2021-10-02 00:52:13 -0300

    Create PointBoard.cs

---
+++ PointBoard.cs
@@ -0,0 +1,43 @@
+/*
+Esse script é basicamente para declarar e inicializar uma variável para cada nível do jogo para armazenar informação sobre a pontuação. 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointBoard : MonoBehaviour
+{
+    public static int level1point = 10;
+    public static int level2point = 10;
+    public static int level3point = 10;
+    public static int level4point = 10;
+    public static int level5point = 10;
+    public static int level6point = 10;
+    public static int level7point = 10;
+    public static int level8point = 10;
+    public static int level9point = 10;
+    public static int level10point = 10;
+    public static int level11point = 10;
+    public static int level12point = 10;
+    public static int level13point = 10;
+    public static int level14point = 10;
+    public static int level15point = 10;
+
+    public static bool level1com = false;
+    public static bool level2com = false;
+    public static bool level3com = false;
+    public static bool level4com = false;
+    public static bool level5com = false;
+    public static bool level6com = false;
+    public static bool level7com = false;
+    public static bool level8com = false;
+    public static bool level9com = false;
+    public static bool level10com = false;
+    public static bool level11com = false;
+    public static bool level12com = false;
+    public static bool level13com = false;
+    public static bool level14com = false;
+    public static bool level15com = false;
+
+
+}
